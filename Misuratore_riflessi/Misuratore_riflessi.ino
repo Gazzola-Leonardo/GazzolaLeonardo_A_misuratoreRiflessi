605dfc7e429ae7dc8d651b1ad63f88735ada1c0b
Gazzola Leonardo <leonardo.gazzola@barsanti.edu.it>
2019-03-22 10:23:33 +0100

    Aggiornamento programma

--- Misuratore_riflessi/Misuratore_riflessi.ino
+++ Misuratore_riflessi/Misuratore_riflessi.ino
@@ -1,40 +1,55 @@
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+
+const int buttonPin = 13;    
+const int ledPin =  12;     
 
 int numRandom = 0;
 int tempo = 0;
 int tempo2 = 0;
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+int buttonState = 0;         
  
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Seriale attiva");
-  // initialize the LED pin as an output:
+  //Serial.begin(9600);
+  //Serial.println("Seriale attiva");
   pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
+  lcd.begin(16, 2);
+  lcd.print("Benvenuto!");
 }
 
 void loop() {
   buttonState = digitalRead(buttonPin); 
   if (buttonState == HIGH) {
-    Serial.println(buttonState);
+    //Serial.println(buttonState);
     accendiLed();
+    tempoLed();
   }
-
+  
 }
 void accendiLed(){
-  numRandom = random(1000, 3000);
-  Serial.println("num random");
-  Serial.println(numRandom);
-  tempo = millis();
+  numRandom = random(1000, 5000) + millis();
+  //Serial.println("num random");
+  //Serial.println(numRandom);
+  tempo = tempo + millis();
   while(true){
     if(numRandom == millis() - tempo){
       digitalWrite(ledPin, HIGH);
-      delay(100);
+      delay(200);
       digitalWrite(ledPin, LOW);
       break;
+    }
+  }
+}
+void tempoLed(){
+  while(true)
+  {
+    buttonState = digitalRead(buttonPin); 
+    if(buttonState == HIGH){
+      lcd.begin(16, 2);
+      lcd.print(millis() - numRandom);
+      //Serial.println(millis() - numRandom);
+      break;      
     }
   }
 }
