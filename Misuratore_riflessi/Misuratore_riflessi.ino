24a1a428bbb926987f8b8a38a9e6c0827546ff98
Gazzola Leonardo <leonardo.gazzola@barsanti.edu.it>
2019-03-22 22:58:05 +0100

    22/03/2019 - Conclusione

--- Misuratore_riflessi/Misuratore_riflessi.ino
+++ Misuratore_riflessi/Misuratore_riflessi.ino
@@ -3,19 +3,23 @@ LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
 const int buttonPin = 13;    
 const int ledPin =  12;   
-const int buzzerPin = 8;
+const int buzzerPin = 11;
+const int rgbRosso = 8;
+const int rgbVerde = 9;
+const int rgbBlu = 10;
 
 double numRandom = 0;
-int tempo = 0;  
+int ledTempo = 0;  
+int buzzerTempo = 0;  
  
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Seriale attiva");
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
   pinMode(buzzerPin, OUTPUT);
-  lcd.begin(16, 2);
-  lcd.print("Benvenuto!");
+  pinMode(rgbRosso, OUTPUT);
+  pinMode(rgbVerde, OUTPUT);
+  pinMode(rgbBlu, OUTPUT);
+  azzera();
 }
 
 void loop() {
@@ -23,23 +27,71 @@ void loop() {
   while(digitalRead(buttonPin) == HIGH);
   accendiLed();
   tempoLed();
-  tone(buzzerPin, 1000, 500);
-  delay(1000);
+  accendiBuzzer();
+  tempoBuzzer();
+  calcolaMedia();
+  while(digitalRead(buttonPin) == HIGH);
+  while(digitalRead(buttonPin) == LOW);
+  azzera();
   while(digitalRead(buttonPin) == HIGH);
 }
 void accendiLed(){
+  lcd.begin(16, 2);
+  lcd.print("Attendi...");
   numRandom = random(1000, 2000) + millis();
   while(numRandom > millis());
   digitalWrite(ledPin, HIGH);
   delay(200);
   digitalWrite(ledPin, LOW);
-  //while(numRandom == millis());
+}
+void accendiBuzzer(){
+  lcd.setCursor(0, 1);
+  lcd.print("Attendi...");
+  numRandom = random(1000, 2000) + millis();
+  while(numRandom > millis());
+  tone(buzzerPin, 1000, 300);
 }
 void tempoLed(){
   while(digitalRead(buttonPin) == LOW);
   lcd.begin(16, 2);
   numRandom = millis() - numRandom;
-  tempo = numRandom;
-  lcd.print(tempo);
+  ledTempo = numRandom;
+  lcd.print("Led: ");
+  lcd.print(ledTempo);
   while(digitalRead(buttonPin) == HIGH);
+}
+void tempoBuzzer(){
+  while(digitalRead(buttonPin) == LOW);
+  lcd.setCursor(0, 1);
+  numRandom = millis() - numRandom;
+  buzzerTempo = numRandom;
+  lcd.print("Buzzer: ");
+  lcd.print(buzzerTempo);
+  while(digitalRead(buttonPin) == HIGH);
+}
+void calcolaMedia(){
+  while(digitalRead(buttonPin) == LOW);
+  lcd.clear();
+  double media = round((ledTempo + buzzerTempo) / 2);
+  lcd.begin(16, 2);
+  lcd.print("Media: ");
+  lcd.print(media);
+  lcd.setCursor(0, 1);
+  if(media < 600){
+    lcd.print("Test passato!!");
+    digitalWrite(rgbVerde, HIGH);
+  }
+  else{
+    lcd.print("Non superato");
+    digitalWrite(rgbRosso, HIGH);
+  }
+  while(digitalRead(buttonPin) == HIGH);
+}
+void azzera(){
+  lcd.begin(16, 2);
+  lcd.print("Benvenuto!");
+  lcd.setCursor(0, 1);
+  lcd.print("Premi il bottone");
+  digitalWrite(rgbRosso, LOW);
+  digitalWrite(rgbVerde, LOW);
 }
